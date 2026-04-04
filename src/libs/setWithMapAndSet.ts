6e141cc1a43516f6929b50f328424504fac8b670
kitakou0313 <kitakou0313@gmail.com>
2026-04-04 16:03:15 +0900

    add: add sample libs

---
+++ src/libs/setWithMapAndSet.ts
@@ -0,0 +1,32 @@
+class CoorinateSetWithMapAndSet {
+    coodinates: Map<number, Set<number>> = new Map()
+
+    add(h:number, w:number) {
+        if (!this.coodinates.has(h)) {
+            this.coodinates.set(h, new Set())
+        }
+        this.coodinates.get(h)?.add(w)
+    }
+
+    has(h:number, w:number):boolean{
+        const wValuesRelatedToh = this.coodinates.get(h)
+
+        if (typeof wValuesRelatedToh === "undefined") {
+            return false
+        }
+
+        return wValuesRelatedToh.has(w)
+    }
+
+    listAllCoordinates():[number, number][] {
+        const allCoodinatesList: [number, number][] = []
+
+        for (const entry of this.coodinates.entries()) {
+            for (const wValue of entry[1]) {
+                allCoodinatesList.push([entry[0], wValue])
+            }
+        }
+
+        return allCoodinatesList
+    }
+}
---
+++ src/libs/setWithString.ts
@@ -0,0 +1,35 @@
+class CoorinateSetWithString {
+    coodinates: Set<string> = new Set()
+
+    convertCoordinateToString(h:number, w:number):string{
+        return `${h}-${w}`
+    }
+
+    convertStringToCoordinate(coordinateString:string):[number, number] {
+        const [hString, wString] = coordinateString.split("-")
+        if (typeof hString === "undefined" || typeof wString === "undefined") {
+            return [NaN, NaN]
+        }
+        return [parseInt(hString, 10), parseInt(wString, 10)]
+    }
+
+    add(h:number, w:number) {
+        const coordinateString = this.convertCoordinateToString(h, w)
+        this.coodinates.add(coordinateString)
+    }
+
+    has(h:number, w:number):boolean{
+        const coordinateString = this.convertCoordinateToString(h, w)
+        return this.coodinates.has(coordinateString)
+    }
+
+    listAllCoordinates():[number, number][] {
+        const allCoodinatesList: [number, number][] = []
+
+        for (const entry of this.coodinates.values()) {
+            const [h, w] = this.convertStringToCoordinate(entry)
+            allCoodinatesList.push([h, w])
+        }
+        return allCoodinatesList
+    }
+}
